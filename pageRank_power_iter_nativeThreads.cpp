f1bf0757c75175a11c8531b53964866b1001f576
ojasmalwankar <omalwank@ucsc.edu>
2021-12-06 22:52:35 -0800

    native threads

--- pageRank_power_iter_nativeThreads.cpp
+++ pageRank_power_iter_nativeThreads.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stdlib.h>
 #include <Eigen/Dense>
+#include <thread>
+#include <vector>
 
 #define MAX_ITER 50
 #define EPSILON 0.0000001
@@ -20,6 +22,21 @@ typedef Eigen::Matrix<long double, Eigen::Dynamic, Eigen::Dynamic> Mat;
 using namespace std;
 using namespace Eigen;
 
+
+void mult(Vec* result, Mat* A, Vec* prevVector, int rstart, int rend, int cstart, int cend, int qstart, int qend) {
+    int r,c,q;
+    for(r = rstart; r <= rend; r++) {
+        for(c = cstart; c <= cend; c++) {
+            if(qstart == 0) {
+                result->coeffRef(r,c) = 0.0;
+            }
+            for(q = qstart; q <= qend; q++) {
+                result->coeffRef(r,c) = result->coeffRef(r,c) + A->coeffRef(r,q) * prevVector->coeffRef(q,c);
+            }
+        }
+    }
+}
+
 bool vector_approx_equal(Vec a, Vec b, double small_value)
 {
     if (a.rows() != b.rows())
@@ -30,12 +47,58 @@ bool vector_approx_equal(Vec a, Vec b, double small_value)
 Vec pageRank_power_iter_modified(Mat A, Vec v_original, Vec tp)
 {
     Vec result(v_original.rows());
+    // Vec ojas_test(v_original.rows());
+    int r_tile_size = 2;
+    int c_tile_size = 2;
+    int q_tile_size = 2;
+    vector<thread> th_vec;
     result.setZero();
     Vec prevVector = v_original;
     for (int i = 0; i < MAX_ITER; i++)
     {
         // A = matrix_power_smart(A, i + 1);
-        result = A * prevVector + tp;
+        // result = A * prevVector + tp;
+        for(int rstart = 0; rstart < A.rows(); rstart += r_tile_size) {
+            int rend = rstart + r_tile_size - 1;
+            if(rend >= A.rows()) {
+                rend = A.rows() - 1;
+            }
+            for(int cstart = 0; cstart < prevVector.cols(); cstart += c_tile_size) {
+                int cend = cstart + c_tile_size - 1;
+                if(cend >= prevVector.cols()) {
+                    cend = prevVector.cols() - 1;
+                }
+                for(int qstart = 0; qstart < A.cols(); qstart += q_tile_size) {
+                    int qend = qstart + q_tile_size - 1;
+                    if(qend >= A.cols()) {
+                        qend = A.cols() - 1;
+                    }
+                    thread th (mult, &result, &A, &prevVector, rstart, rend, cstart, cend, qstart, qend);
+                    th_vec.push_back(move(th));
+                    if(th_vec.size() == 10) {
+                        for(int i = 0; i < th_vec.size(); i++) {
+                            th_vec[i].join();
+                        }
+                        th_vec.clear();
+                    }
+                    if(qend = A.cols() - 1) {
+                        for(int i = 0; i < th_vec.size(); i++) {
+                            th_vec[i].join();
+                        }
+                        th_vec.clear();
+                    }
+                }
+            } 
+        } // end of final for
+        for(int i = 0; i < th_vec.size(); i++) {
+            th_vec[i].join();
+        }
+        th_vec.clear();
+        // ojas_test = ojas_test + tp;
+        result = result + tp;
+        // cout << ojas_test << endl;
+        // cout << "result \n" << endl;
+        // cout << result << endl;
         if (vector_approx_equal(result, prevVector, EPSILON))
         {
             cout << "Converged @ Iteration " << i << ": " << result.transpose() << endl;
